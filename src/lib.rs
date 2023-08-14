ea3ac94a5f53e901b0ac658483bb56221e90fa3e
Shaurya Arora <shaurya.arora1@gmail.com>
2023-08-13 21:31:58 -0400

    Remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -69,18 +69,3 @@ where
         results
     }
 }
-
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
